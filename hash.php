ce8a521d117433e493fc8dd850b2faceccdc1b61
Kenke19 <kenke2003@gmail.com>
2025-07-16 05:31:44 +0100

    second commit

--- hash.php
+++ hash.php
@@ -1,7 +1,6 @@
 <?php
-$password = 'Mandy12'; // Username- Admin, pass - Admin@12
-echo password_hash($password, PASSWORD_DEFAULT);
+// Username- Admin, pass - Admin@12
+
 // Username- Nkechi, pass - Kenke19, email - kenketuka@gmail.com
 // Username- kenke, pass - mandy, email - amanda4liquor@gmail.com
 // Username- Test User, pass - password, email - test@example.com
-// $2y$10$1Y/CDhKdx5nRF317gE6AyexlfX7fiKnRkMYlSKvJBpnadCEswL4yC 
